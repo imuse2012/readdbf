ff87289d3b773acc22baf1fdd776f0ec5ec1475a
Your Name <you@example.com>
2014-05-22 17:54:57 +0800

    init

---
+++ test2.go
@@ -0,0 +1,35 @@
+package main
+
+import(
+  "fmt"
+  "readdbf/go-dbf/godbf"
+)
+
+
+func main(){
+
+    file1 := "show2003.dbf"
+//    file1 := "sjshq.dbf"
+
+    dbfTable, e := godbf.NewFromFile(file1, "GB2312") 
+    if e != nil {
+
+        fmt.Println(e)
+    }
+    fields := dbfTable.Fields()
+
+    fmt.Println(fields)
+
+    for _,v := range fields {
+        fmt.Println(v.FieldName())
+    }
+
+    for i := 0; i< dbfTable.NumberOfRecords(); i++{
+
+        row := dbfTable.GetRowAsSlice(i)
+        fmt.Println(row)
+
+    }
+
+    fmt.Println(dbfTable.NumberOfRecords())
+}
